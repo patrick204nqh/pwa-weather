a20bd1340e62a184f5960397abcbbbe527e549cd
patrick <patrick204nqh@gmail.com>
2020-06-07 09:59:29 +0700

    show info weather

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 const App = () => {
   const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
   const search = async (e) => {
     if (e.key === 'Enter') {
       const data = await fetchWeather(query);
 
-      console.log(data);
+      setWeather(data);
+      setQuery('');
     }
   };
 
@@ -24,6 +26,26 @@ const App = () => {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
+      {weather.main && (
+        <div className='city'>
+          <h2 className='city-name'>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+            <div className='city-temp'>
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className='info'>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+                className='city-icon'
+              />
+              <p>{weather.weather[0].main}</p>
+            </div>
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
